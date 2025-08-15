e3f939958c8153c5759bc3f3b3400f511a0c01c7
Thanapat <asiajek@gmail.com>
2025-08-16 03:41:26 +0700

    [ADD] integrate Tailwind CSS and set up initial routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './feutures/home/home';
+import { Dashboard } from './feutures/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: Home
+	},
+	{
+		path: 'dashboard',
+		component: Dashboard
+	}
+];
