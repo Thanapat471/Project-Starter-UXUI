4d051538b7364e2c89675852d99f7316188bbe86
sittinon <sittinon.to@email.com>
2025-08-28 00:21:47 +0700

    Angular 20.2 + PrimeNG 20 setup

--- src/app/feutures/home/home.ts
+++ src/app/feutures/home/home.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, ButtonModule, TableModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {}
+export class Home {
+  orders = [
+    { id: 1, product: 'Apple', quantity: 3 },
+    { id: 2, product: 'Banana', quantity: 5 },
+    { id: 3, product: 'Orange', quantity: 2 }
+  ];
+}
